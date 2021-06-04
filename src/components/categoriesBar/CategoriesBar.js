b97fff73a7925e86c3ff01bd12a023e232bd2d15
rebeljeet <jeetpatel7879@gmail.com>
2021-06-04 23:57:30 +0530

    Design Categories Bar

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './_categoriesBar.scss';
 
+
+const keywords = [
+    'All',
+    'React js',
+    'Angular js',
+    'React Nativ',
+    'Use of Api',
+    'Redux',
+    'Music',
+    'Algorithm Art',
+    'Guitar',
+    'Bengali Songs',
+    'Coding',
+    'Cricket',
+    'Football',
+    'Real Madrid',
+    'Gatsby',
+    'Poor Coder',
+    'Shwetabh',
+]
+
+
+
 const CategoriesBar = () => {
-    return (
-        <div>
-            Bar
-        </div>
-    )
+
+    const [activeElement, setActiveElement] = useState('All')
+
+    const handleClick = (value) => {
+        setActiveElement(value)
+    }
+
+    return <div className="CategoriesBar">
+
+        {
+            keywords.map((value, i) => (
+                <span onClick={() => handleClick(value)} key={i}
+                    className={activeElement === value ? 'active' : ''} >
+
+                    {value}
+                </span>
+            ))
+        }
+
+
+    </div>
 }
 
 export default CategoriesBar
