4d506a5976095827ab4e2421646bce247afb9fbc
rebeljeet <jeetpatel7879@gmail.com>
2021-06-08 23:11:29 +0530

    firebase configuration with youtube API

---
+++ src/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+console.log(process.env.REACT_APP_YT_API_KEY)
+const request = axios.create({
+    baseURL : "https://youtube.googleapis.com/youtube/v3/",
+    params : {
+        key: "AIzaSyDJfHiijWyDEELT8n5v-zufhadwLUnjTrE",
+    },
+})
+
+export default request
--- src/screens/homeScreen/HomeScreen.js
+++ src/screens/homeScreen/HomeScreen.js
@@ -1,19 +1,29 @@
-import React from 'react'
-import { Container, Row ,Col} from 'react-bootstrap';
+import React, { useEffect } from 'react'
+import { Container, Row, Col } from 'react-bootstrap';
 import Video from '../../components/video/Video'
 import CategoriesBar from '../../components/categoriesBar/CategoriesBar';
+import { useDispatch } from 'react-redux';
+import { getPopularVideos } from '../../redux/actions/videos.action';
 
 
 const HomeScreen = () => {
+
+    const dispatch = useDispatch()
+    useEffect(() => {
+        dispatch(getPopularVideos())
+
+    }, [dispatch])
+
+
     return (
         <Container>
             <CategoriesBar />
             <Row>
                 {
-                    [...new Array(20)].map(()=>(
-                    <Col lg={3} md={4}>
-                    <Video />
-                    </Col>))
+                    [...new Array(20)].map(() => (
+                        <Col lg={3} md={4}>
+                            <Video />
+                        </Col>))
                 }
             </Row>
         </Container>
